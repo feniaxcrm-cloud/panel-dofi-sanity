589a82b6639b7cdb67f21f099f858b76f8839df0
feniaxcrm-cloud <feniax.crm@gmail.com>
2026-08-19 12:59:56 -0500

    feat: separar panel administrativo por secciones independientes

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,4 +1,13 @@
 import servicio from './servicio'
-import cliente from './cliente' // <-- Nueva importación
+import cliente from './cliente' 
+import seccionHacemos from './seccionHacemos'// <-- Nueva importación
+import seccionSocio from './seccionSocio'// <-- Nueva importación
+import seccionCierre from './seccionCierre'// <-- Nueva importación
 
-export const schemaTypes = [servicio, cliente]
+export const schemaTypes = [
+  servicio, 
+  cliente, 
+  seccionHacemos, 
+  seccionSocio, 
+  seccionCierre
+]
