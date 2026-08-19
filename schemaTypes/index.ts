94298506ad5756f7505c4f5e4e7c64b18f2ffaf8
feniaxcrm-cloud <feniax.crm@gmail.com>
2026-08-19 15:37:24 -0500

    feat: agregar esquema para pagina inicio

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -4,8 +4,10 @@ import seccionHacemos from './seccionHacemos'
 import seccionSocio from './seccionSocio'
 import seccionCierre from './seccionCierre'
 import seccionPilares from './seccionPilares' // <-- Nueva importación
+import paginaInicio from './paginaInicio'
 
 export const schemaTypes = [
+  paginaInicio,
   servicio, 
   cliente, 
   seccionHacemos, 
